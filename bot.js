76ae8cdd07496cf42d2e7db206822174bef1f719
Ananya Srinivasan <aasrin99@gmail.com>
2018-11-28 14:36:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,12 +83,12 @@ const messageListener = message => {
    */
   if (message['sender_type'] === 'user') {
     // Checks if the sent message contained the string 'bot'.
-    if (message['text'].indexOf('bot') !== -1) {
+    if (message['text'].indexOf('Smitty') !== -1) {
       /* Sends a GroupMe message as this bot to the bot's
        * group, indicating its insecurity about the topic
        * of conversation.
        */
-      sendMessage('OMG! Are you guys gossiping about me?!?!')
+      sendMessage('He was number 1!')
     }
   }
 }
